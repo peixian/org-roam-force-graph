92a2567bf76a26fc8d6d711dd4e3aeccd9cc7c48
Peixian Wang <peixian@space.af>
2020-12-19 18:18:30 -0500

    add readme and finalize

--- org-roam-d3.py
+++ org-roam-d3.py
@@ -3,9 +3,11 @@ import networkx as nx
 import sqlite3
 import logging
 import json
+import argparse
+from pathlib import Path
 from cdlib.algorithms import leiden
 
-def load_from_db(path="/Users/peixian/.emacs.d/org-roam.db"):
+def load_from_db(path):
     logging.info(f"Loading from {path}")
     conn = sqlite3.connect(path)
     c = conn.cursor()
@@ -28,16 +30,18 @@ def load_from_db(path="/Users/peixian/.emacs.d/org-roam.db"):
 
     return t, links
 
-def generate_url(file_name):
-    return file_name.replace("/Users/peixian/code/org/", "https://wiki.space.af/posts/").replace(".org", "")[1:-1]
+def generate_url(file_name, replace_dict):
+    new_url = file_name
+    for key, val in replace_dict.items():
+        if val == "NONE":
+            val = ""
+        new_url = new_url.replace(key, val)
+    return new_url
 
-def parse_links(links, titles):
+def parse_links(links, titles, replace_dict={}):
     logging.info(f"Parsing links")
     l = []
     for file1, file2, kind, _ in links:
-        print(file1)
-        print(file2)
-
         if 'private' in file2 or 'private' in file2:
             continue
         file1_name = titles.get(file1)
@@ -45,9 +49,9 @@ def parse_links(links, titles):
         if file1_name and file2_name:
             l.append({
                 "source": file1_name,
-                "source_url": generate_url(file1),
+                "source_url": generate_url(file1, replace_dict),
                 "target": file2_name,
-                "target_url": generate_url(file2),
+                "target_url": generate_url(file2, replace_dict),
                 "type": kind[1:-1]
             })
     return l
@@ -94,7 +98,7 @@ def generate_community_colors(links, community_algo=leiden):
     nodes = color_nodes(community_sets, links)
     return nodes
 
-def dump(nodes, links, name='./org-data.json'):
+def dump(nodes, links, name):
     logging.info(f"Writing json to {name}")
     output = {}
     output['links'] = links
@@ -102,7 +106,16 @@ def dump(nodes, links, name='./org-data.json'):
     with open(name, 'w') as f:
         json.dump(output, f)
 
+
+
 if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Generates a json file from your org-roam DB")
+    parser.add_argument("--org-db-location",  help="Location of org-roam.db file. Defaults to $HOME/.emacs.d/org-roam.db", type=str, default=f"{Path.home()}/.emacs.d/org-roam.db", dest="db_location")
+    parser.add_argument("--output", "-o", help="File to output as. Defaults to './org-data.json'", type=str, default="./org-data.json", dest="output_location")
+    parser.add_argument("--replace", dest="replacements", nargs="+", help="Replacement to generate urls. Takes in <FILE_PATH> <REPLACEMENT_VALUE>")
+
+    args = parser.parse_args()
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
@@ -111,7 +124,14 @@ if __name__=="__main__":
             logging.StreamHandler()
         ]
     )
-    titles, links = load_from_db()
-    links = parse_links(links, titles)
+
+    if len(args.replacements) % 2 != 0:
+        print("Replacements must be in pairs")
+        exit(1)
+
+    titles, links = load_from_db(path=args.db_location)
+    replacements = {args.replacements[i]: args.replacements[i+1] for i in range(0, len(args.replacements), 2)}
+    logging.info(f"Replacing according to {replacements}")
+    links = parse_links(links, titles, replacements)
     nodes = generate_community_colors(links)
-    dump(nodes, links)
+    dump(nodes, links, name=args.output_location)
